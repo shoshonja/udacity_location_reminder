76800be119e085cbc09d2abfff41d9c118178ef6
ivansosic <ivan.sosic@greyp.com>
2023-05-03 14:17:13 +0200

    added fake data source

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -2,27 +2,34 @@ package com.udacity.project4.locationreminders.data
 
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
-
-//    TODO: Create a fake data source to act as a double to the real data source
+class FakeDataSource(var reminders: MutableList<ReminderDTO>? = mutableListOf()) :
+    ReminderDataSource {
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        reminders?.let { return Result.Success(ArrayList(it)) }
+        return Result.Error(
+            "No Reminders!"
+        )
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        reminders?.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        reminders?.let {
+            for (reminder in it) {
+                if (reminder.id == id)
+                    return Result.Success(reminder)
+            }
+        }
+        return Result.Error("Reminder not found!")
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        reminders?.clear()
     }
-
-
 }
--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -3,12 +3,58 @@ package com.udacity.project4.locationreminders.savereminder
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class SaveReminderViewModelTest {
 
+    @Test
+    fun onClear_completes_dataIsNull() {
+
+    }
+
+    @Test
+    fun propagatePoiData_emptyPoi_emptyLiveData() {
+
+    }
+
+    @Test
+    fun propagatePoiData_existingPoi_populatedLiveData() {
+
+    }
+
+    @Test
+    fun validateEnteredData_emptyTitle_returnsFalse() {
+
+    }
+
+    @Test
+    fun validateEnteredData_emptyLocation_returnsFalse() {
+
+    }
+
+    @Test
+    fun validateEnteredData_validData_returnsTrue() {
+
+    }
+
+    //This is tough one - fake base and fakes data source
+    @Test
+    fun saveReminder_completes_dataSaved(){
+
+    }
+
+    @Test
+    fun validateAndSave_validData_validDataIsSaved(){
+
+    }
+
+    @Test
+    fun validateAndSave_invalidData_saveNotCalled(){
+
+    }
 
     //TODO: provide testing to the SaveReminderView and its live data objects
 
